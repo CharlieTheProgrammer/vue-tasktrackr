86f17ae0791a052ceeef942c246b397a4bb6327d
Charlie Ontiveros <dev@charlietheprogrammer.com>
2021-05-25 16:38:02 -0700

    Completed html portion of the footer component.

---
+++ src/components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+	<footer class="footer">
+		<div class="p-2 text-gray-100 text-sm text-center font-semibold">
+			<span>&copy; {{ year }} Charlie The Programmer</span>
+		</div>
+	</footer>
+</template>
+
+<script>
+export default {
+	computed: {
+		year() {
+			return new Date().getFullYear();
+		},
+	},
+};
+</script>
+
+<style></style>
