84ef9d0b8e0fe6344400a3376c5f5c789facff24
Charlie Ontiveros <dev@charlietheprogrammer.com>
2021-05-25 20:36:42 -0700

    Added routes and stub components.

---
+++ src/router/index.js
@@ -0,0 +1,67 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/about",
+    name: "About",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+    component: () =>
+      import(/* webpackChunkName: "dashboard" */ "../views/Dashboard.vue"),
+  },
+  {
+    path: "/docs",
+    name: "Docs",
+    component: () =>
+      import(/* webpackChunkName: "docs" */ "../views/Docs.vue"),
+  },
+  {
+    path: "/forgot-password",
+    name: "ForgotPassword",
+    component: () =>  
+      import(/* webpackChunkName: "forgot-password" */ "../views/ForgotPassword.vue"),
+  },
+  {
+    path: "/login",
+    name: "Login",  
+    component: () =>
+      import(/* webpackChunkName: "login" */ "../views/Login.vue"),
+  },
+  {
+    path: "/projects",
+    name: "Projects",
+    component: () =>
+      import(/* webpackChunkName: "projects" */ "../views/Projects.vue"),
+  },
+  {
+    path: "/settings",
+    name: "Settings",
+    component: () =>
+      import(/* webpackChunkName: "settings" */ "../views/Settings.vue"),
+  },
+  {
+    path: "/workspace",
+    name: "Workspace",
+    component: () =>
+      import(/* webpackChunkName: "workspace" */ "../views/Workspace.vue"),
+  },
+];
+
+const router = new VueRouter({
+  routes,
+});
+
+export default router;
---
+++ src/views/ForgotPassword.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>This is an forgot password page</h1>
+  </div>
+</template>
