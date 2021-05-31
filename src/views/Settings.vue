470e449d772d6caca80a8802d5acf61ce8341231
Charlie Ontiveros <dev@charlietheprogrammer.com>
2021-05-30 21:22:38 -0700

    Created Settings view. Still a WIP.

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,5 +1,22 @@
 <template>
-  <div class="about">
-    <h1>This is an settings page</h1>
-  </div>
+	<div class="max-w-4xl mx-auto w-full">
+		<h1 class="text-5xl font-light text-gray-400 mb-6">Settings</h1>
+		<div class="space-y-14">
+			<v-projects-list></v-projects-list>
+
+			<v-categories-list></v-categories-list>
+		</div>
+	</div>
 </template>
+
+<script lang="ts">
+import ProjectsListVue from '@/components/ProjectsList.vue';
+import CategoriesListVue from '@/components/CategoriesList.vue';
+
+export default {
+	components: {
+		'v-projects-list': ProjectsListVue,
+		'v-categories-list': CategoriesListVue,
+	}
+};
+</script>
